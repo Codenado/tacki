d7072c6488eeb2f75f086571f6d73923a09c0430
Jeffrey Leonard <jeffrey9279@gmail.com>
2016-03-09 11:30:57 -0800

    first

---
+++ client/app/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('tackiApp', [
+  'tackiApp.auth',
+  'tackiApp.admin',
+  'tackiApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap',
+  'validation.match'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
